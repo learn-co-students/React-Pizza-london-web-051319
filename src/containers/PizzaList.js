c2ff640fdae71f2361f792ced3c6284ca613a3a2
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-07-29 11:02:46 +0100

    Done.

--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -15,7 +15,7 @@ class PizzaList extends Component {
         </thead>
         <tbody>
           {
-            //render Pizza here
+           this.props.pizzas.map(pizza => <Pizza key={pizza.id} pizza={pizza} selectPizza={this.props.selectPizza}/>)
           }
         </tbody>
       </table>
